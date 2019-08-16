66af68419838626981b3ff299efa6b78189ff286
geeks121 <yozhy999@gmail.com>
2019-08-16 16:31:30 +0700

    Update jquery.pietimer.js

--- jquery.pietimer.js
+++ jquery.pietimer.js
@@ -13,7 +13,7 @@
     init: function(options) {
       var state = {
         timer: null,
-        timerSeconds: 10,
+        timerSeconds: 35,
         callback: function() {},
         timerCurrent: 0,
         showPercentage: false,
